65c5bb72be5a2883e0a05dbb8fc77464811dd593
nagolove <dekar.own@gmail.com>
2024-07-08 02:30:01 +0300

    Parallel graph rendering.

--- build_run.sh
+++ build_run.sh
@@ -4,6 +4,9 @@ set -Exeuo pipefail
 
 gcc -o huff_test ./huff_test.c -fsanitize=address -g3
 
+# временно отключить выход из скрипта при получении не нулевого кода возврата
+#(set +e; ./huff_test; set -e)
+
 ./huff_test
 
 #files=(
@@ -15,7 +18,8 @@ gcc -o huff_test ./huff_test.c -fsanitize=address -g3
 readarray -d '' files < <(fd -0 ".*\.dot")
 
 for file_in in "${files[@]}"; do
-    #file_out="${file_in%.dot}.png"
     file_out="${file_in%.dot}.png"
-    dot -Tpng -o $file_out $file_in
+    dot -Tpng -o $file_out $file_in &
 done
+
+wait
